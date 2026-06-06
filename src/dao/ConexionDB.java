cc468bb37a2e687c3f9bdb9c77f6e1492288252b
Brayan Espinosa Osorio <228817178+bespinosaosorio96-ship-it@users.noreply.github.com>
2026-06-05 20:52:25 -0500

    Update ConexionDB.java

--- src/dao/ConexionDB.java
+++ src/dao/ConexionDB.java
@@ -9,8 +9,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 /**
- *
- * @author PC_WIN11
+    private static final String URL = "jdbc:mysql://mysql-98caa95-bespinosaosorio96-0c80.b.aivencloud.com:20399/tecnostore_db";
+    private static final String USER = "avnadmin";
+    private static final String PASSWORD = "AVNS_nTafLI46V3U-bV6e29N";
  */
 public class ConexionDB {
     private static final String URL = "jdbc:mysql://mysql-3e32b359-aprendiendomysql.b.aivencloud.com:26338/tecnostore_db";      // "jdbc:mysql://localhost:3306/tecnostore_db";
